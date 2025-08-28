2d7ac3badf9f98a0b20450229db3cbdf4e1bcb03
DebabrataBanik <debabratabanik02@gmail.com>
2025-08-28 19:58:17 +0530

    chore: initialize React project with TypeScript, Tailwind, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div>Hello Weather App.</div>
+      <Button className="ml-10" variant='destructive'>Choke Me</Button>
+    </>
+  )
+}
+
+export default App
